aa29cb93d6877292370d1a68e5768f6bb2705b66
KeilCarpenter <keil@keilcarpenter.co.nz>
2017-02-03 10:05:25 +1300

    cleanup

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,8 +1,6 @@
-import { Component, OnInit, EventEmitter, Output} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Recipe } from '../../../models/recipe';
 import { RecipeService } from '../../../services/recipe.service';
-import {Ingredient} from "../../../models/ingredient";
-
 
 @Component({
   selector: 'rb-recipe-list',
@@ -10,22 +8,12 @@ import {Ingredient} from "../../../models/ingredient";
   styleUrls: ['recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
-  recipes: Recipe[] = []; /** Array of Recipes */
-
-  /*Trigger new event*/
-  @Output() recipeSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [];
 
   constructor(private recipeService: RecipeService){}
 
   ngOnInit() {
       this.recipes = this.recipeService.getRecipes();
-  }
-
-  onSelected(recipe: Recipe){
-    /*Push selected recipe to even emitter*/
-    this.recipeSelected.emit(recipe)
-    console.log(this);
   }
 
 }
