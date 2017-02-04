0b91b096c53ec48b8f039960408f9f86d4608d6a
KeilCarpenter <keil@keilcarpenter.co.nz>
2017-02-04 16:31:10 +1300

    Manage recipe ingredients

--- src/app/components/recipe-edit/recipe-edit.component.ts
+++ src/app/components/recipe-edit/recipe-edit.component.ts
@@ -63,6 +63,22 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
   onCancel(){
     this.navigateBack();
   }
+
+  onAddItem(name: String, amount: String){
+    (<FormArray>this.recipeForm.controls['ingredients']).push(
+      new FormGroup({
+        name: new FormControl(name, Validators.required),
+        amount: new FormControl(amount, [
+          Validators.required,
+          Validators.pattern("\\d+")
+        ])
+      })
+    );
+  }
+
+  onRemoveItem(index: number){
+    (<FormArray>this.recipeForm.controls['ingredients']).removeAt(index);
+  }
   // Prevent memory leaks
   ngOnDestroy() {
     this.subscription.unsubscribe();
